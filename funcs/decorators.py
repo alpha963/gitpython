cc660c309b99836f50f5065cecd0cb73592ba13e
wangqh <alpha_963@163.com>
2015-04-12 20:16:20 +0800

    funcs/

---
+++ funcs/decorators.py
@@ -0,0 +1,50 @@
+import functools
+
+def log(func):
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def nows():
+	print '2013-12-25'
+
+f=nows
+f()
+
+print '\n',nows.__name__
+print f.__name__
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+def loges(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+def logess(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print '2014-12-25'
+
+print '\n',now()
+t=now
+print t.__name__
